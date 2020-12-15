973ace943c33184e81db633a3dedebfcd4a2f313
Winnie Otieno <winnieotieno770@gmail.com>
2020-12-16 00:04:45 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React, {useEffect, useState} from 'react';
+ 
+import './App.css';
+
+import Chuck  from './chuck.png'
+
+import axios from 'axios'
+
+function App() {
+  const [state, setState] = useState({
+    joke:''
+  })
+  const [query, queryState] = useState('teeth');
+
+useEffect(()=>{
+
+  fetchData();
+  getResult()
+
+},[])
+
+const fetchData = async() => {
+  const response = await axios.get('https://api.chucknorris.io/jokes/random')
+  console.log(response.data.value);
+  setState({
+    ...state,
+    joke:response.data.value
+  })
+  
+}
+const getResult = async() => {
+  const result = await axios.get(`c`)
+  console.log(result.data.value)
+  
+}
+
+
+  return (
+    <div className='container'>
+        <div className="row">
+          <div className="col-6">
+            <h1 className='title'>CHUCKNORRIS API</h1>
+            <img src={Chuck} alt="Chuck Norris"/>
+          </div>
+          <div className="col-6">
+            <div className="card">
+              <div className="card-header">
+                Search for a word
+              </div>
+              <div className="card-body">
+                <input type='text'/>
+              </div>
+            </div>
+            <div>
+          <button className="btn btn-warning" onClick={fetchData}>Generate Joke</button>
+        </div>
+          </div>
+
+  <h3 className="subTitle">{state.joke}</h3>
+        </div>
+    
+    </div>
+      );
+}
+
+export default App;
